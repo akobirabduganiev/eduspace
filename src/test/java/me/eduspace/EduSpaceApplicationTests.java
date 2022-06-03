d1a9e0321a861fd4cd41c4b4ed2df381d3fe0013
Akobir Abduganiev <akobir.abduganiev@ya.ru>
2022-06-04 02:12:42 +0500

    Initial commit

---
+++ src/test/java/me/eduspace/EduSpaceApplicationTests.java
@@ -0,0 +1,13 @@
+package me.eduspace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduSpaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
