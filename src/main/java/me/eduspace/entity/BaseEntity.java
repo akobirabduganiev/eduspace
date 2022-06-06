69cb7b3acd60c3788cb72aec56ba785397b3f2d6
Bakhramov <bahromovjamshid503@gmail.com>
2022-06-06 11:10:21 +0500

    added fix BaseEntity

--- src/main/java/me/eduspace/entity/BaseEntity.java
+++ src/main/java/me/eduspace/entity/BaseEntity.java
@@ -1,15 +1,22 @@
 package me.eduspace.entity;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.springframework.data.annotation.LastModifiedDate;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
+@Getter
+@Setter
 @MappedSuperclass
 public class BaseEntity {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @CreationTimestamp
